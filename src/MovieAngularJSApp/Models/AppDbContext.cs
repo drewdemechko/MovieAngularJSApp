9954ada57ad26a5f31f1c794614fb64a2112f749
Drew Demechko <drew_demechko@yahoo.com>
2016-05-10 01:04:59 -0500

    Added blank AngularJS/.Net MVC app

---
+++ src/MovieAngularJSApp/Models/AppDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Data.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieAngularJSApp.Models
+{
+    public class AppDbContext : DbContext
+    {
+        private static bool _created = false;
+
+        public AppDbContext()
+        {
+            if (!_created)
+            {
+                _created = true;
+                Database.EnsureCreated();
+            }
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+        }
+
+        //public DbSet<ModelClassName> Name { get; set; }
+    }
+}
